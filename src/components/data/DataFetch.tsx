f3aab98080a0a19758b3f977d5f49b61073a08b8
Oluwaseun Oyagbile <oyagbileoluwaseun@gmail.com>
2026-04-08 08:26:10 +0100

    added data folder

--- src/components/data/DataFetch.tsx
+++ src/components/data/DataFetch.tsx
@@ -7,19 +7,31 @@ type Product = {
   price: number;
   category: string;
 };
+const PRODUCTS_API_URL = "https://dummyjson.com/products";
 
 function DataFetch() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchProducts(): Promise<void> {
-      const response = await fetch("https://dummyjson.com/products");
+      const response = await fetch(PRODUCTS_API_URL);
       const data = await response.json();
       setProducts(data.products);
+      setLoading(false);
     }
 
     fetchProducts();
   }, []);
+
+  if (loading) {
+    return (
+        <main style={{ padding: "24px", fontFamily: "Arial, sans-serif" }}>
+        <h1>DummyJSON Products</h1>
+        <p>Loading products...</p>
+      </main>
+    );
+  }
 
   return (
     <main style={{ padding: "24px", fontFamily: "Arial, sans-serif" }}>
