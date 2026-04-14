04ba223a3048e8572ab799a93e7b222c9fa3cfb7
Oluwaseun Oyagbile <oyagbileoluwaseun@gmail.com>
2026-04-14 22:11:31 +0100

    updated

--- src/components/data/DataFetchAxios.tsx
+++ src/components/data/DataFetchAxios.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "../search/SearchBar";
 import CategoryFilter from "../search/CategoryFilter";
 import ProductList from "../List/ProductList";
@@ -8,22 +8,31 @@ import useFetchAxios from "../../hooks/useFetechAxios";
 function DataFetchAxios() {
   const [category, setCategory] = useState<string>("smartphones");
   const [search, setSearch] = useState<string>("");
+  const [debounceSearch, setDebounceSearch] = useState<string>("");
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebounceSearch(search);
+    }, 500);
+
+    return () => clearTimeout(timerId);
+  }, [search]);
 
   const requestFn = async (signal: AbortSignal) => {
-    if (search.trim()) {
-      return searchProducts(search, signal);
+    if (debounceSearch.trim()) {
+      return searchProducts(debounceSearch, signal);
     }
     return fetchProductsByCategory(category, signal);
     };
 
-    const { data, loading, error } = useFetchAxios(requestFn, [category, search]);
+    const { data, loading, error } = useFetchAxios(requestFn, [category, debounceSearch]);
 
     const products = data ? data.products : [];
 
     return ( 
     <main style={{ padding: "24px", fontFamily: "Arial, sans-serif" }}>
       <h1>Products Explorer: {category}</h1>
-      <h4>Searching for: {search}</h4>
+      <h4>Searching for: {debounceSearch}</h4>
 
         {/* Search Input */}
         <SearchBar search={search} setSearch={setSearch} />
